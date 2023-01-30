c8a95b955c4166298629bb80a6a8a166c3dd6b6d
RaresColf <111371342+RaresColf@users.noreply.github.com>
2023-01-30 18:58:18 +0200

    Add files via upload

---
+++ src/main/java/ro/sda/book_app/model/Book.java
@@ -0,0 +1,26 @@
+package ro.sda.book_app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@Entity
+@Table(name = "books")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String author;
+    @Column(name = "publication_year")
+    private int year;
+}
+
+
